470c65f234ecf71bd3310f7429e641f4b196d296
amemome <meltksq5140@gmail.com>
2024-05-23 19:33:14 +0900

    config file edit
    뭔가 추가해야됨

---
+++ back-repo/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:8081")  // Vue.js 개발 서버의 포트
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+}
