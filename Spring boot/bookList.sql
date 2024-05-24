abbbd6a497d9a276dc6ae8bb85d014c7f9e5fbf7
Coffeecaat <139877290+Coffeecaat@users.noreply.github.com>
2024-05-25 02:43:36 +0900

    Add files via upload

---
+++ Spring boot/bookList.sql
@@ -0,0 +1,19 @@
+use library;
+
+DROP PROCEDURE IF EXISTS testDataInsert;
+
+DELIMITER $$
+
+CREATE PROCEDURE testDataInsert()
+BEGIN
+	DECLARE i INT DEFAULT 1;
+    
+    WHILE i <= 120 DO
+		INSERT INTO book_list(title, content)
+			VALUES(concat('제목',i), concat('내용',i));
+		SET i= i+ 1;
+	END WHILE;
+END$$
+DELIMITER $$
+
+call testDataInsert;
---
+++ Spring boot/libro/src/main/java/com/library/libro/entity/book_list.java
@@ -0,0 +1,26 @@
+package com.library.libro.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class book_list {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private String content;
+
+
+
+
+
+
+
+}
